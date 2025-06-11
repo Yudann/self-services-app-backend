c44548eac173fbc36c83a184f8c715f5cfa3372a
Yudann <yudannnnn80@gmail.com>
2025-06-12 00:27:25 +0700

    feat: allow cors origin

--- src/main/java/com/kelompok2/selfservicesapp/controller/TransactionController.java
+++ src/main/java/com/kelompok2/selfservicesapp/controller/TransactionController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -46,7 +47,7 @@ public class TransactionController {
                 new ApiResponse<>(200, "Success", transactions)
         );
     }
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/checkout")
     public ResponseEntity<ApiResponse<Transactions>> checkout(@RequestBody CheckoutRequest request) {
         // Cari customer
